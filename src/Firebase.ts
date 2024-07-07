6cb73174131729b75abaf8303139c29acc536dc4
jin <jinyong.suh@navercorp.com>
2024-07-08 00:11:42 +0900

    배포까지 성공

---
+++ src/Firebase.ts
@@ -0,0 +1,71 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { action, makeAutoObservable } from "mobx";
+
+class Firebase {
+  private static readonly config = {
+    apiKey: "AIzaSyBhg7PCSJY7Zm6p804Y5dTad4Qoi8Tr6MU",
+    authDomain: "lessismore-7e070.firebaseapp.com",
+    projectId: "lessismore-7e070",
+    storageBucket: "lessismore-7e070.appspot.com",
+    messagingSenderId: "434364025032",
+    appId: "1:434364025032:web:a8c458d1ee31b0e14dbdfd",
+    measurementId: "G-NC0J0766BX",
+  };
+
+  private auth: any;
+  private loggedIn = false;
+  private userId = "";
+
+  public constructor() {
+    makeAutoObservable(this);
+  }
+
+  public initialize() {
+    const fireBaseApp = initializeApp(Firebase.config);
+    this.auth = getAuth(fireBaseApp);
+
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+
+        console.log(uid);
+        this.userId = user.email ?? "";
+        this.loggedIn = true;
+        // ...
+      } else {
+        this.loggedIn = false;
+        // User is signed out
+        // ...
+      }
+    });
+  }
+
+  public async createUserWithEmailAndPassword(email: string, password: string) {
+    try {
+      await createUserWithEmailAndPassword(this.auth, email, password);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async login(email: string, password: string) {
+    await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  public getUserId() {
+    return this.userId;
+  }
+}
+const firebase = new Firebase();
+firebase.initialize();
+export default firebase;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { babel } from "@rollup/plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+    }),
+  ],
 });
