055657adde810d04a71e1fdf05b7a21ff58f3c19
jinyong.suh <jinyong.suh@navercorp.com>
2024-07-28 23:37:10 +0900

    이미지 수정

--- src/bag/Bag.tsx
+++ src/bag/Bag.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
-import firebase from "../firebase/Firebase.ts";
 import Top from "../Top.tsx";
 
 const Bag = () => {
   return (
     <>
       <Top />
-      <img src={"src/assets/bag.png"} />
+      <img src={"https://www.useless.my/bag.png"} alt="Bag" />
     </>
   );
 };
