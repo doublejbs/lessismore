513a2db985349da8a1a9d27c99c6dff103cad660
jin <jinyong.suh@navercorp.com>
2025-01-18 16:21:11 +0900

    abstract 사용

--- src/search-warehouse/SearchWarehouse.ts
+++ src/search-warehouse/SearchWarehouse.ts
@@ -1,58 +1,25 @@
-import { makeAutoObservable } from 'mobx';
 import Gear from './Gear';
 import app from '../App';
 import GearStore from '../firebase/GearStore';
+import Search from './Search';
 
-class SearchWarehouse {
+class SearchWarehouse extends Search {
   public static new() {
     return new SearchWarehouse(app.getGearStore());
   }
 
-  private keyword: string = '';
-  private result: Array<Gear> = [];
-
-  private constructor(private readonly gearStore: GearStore) {
-    makeAutoObservable(this);
-  }
-
-  public async getAll() {
-    this.setResult(await this.gearStore.searchAll());
-  }
-
-  public async search(keyword: string) {
-    this.setKeyword(keyword.trim());
-
-    if (this.keyword) {
-      this.setResult(await this.gearStore.searchList(this.keyword));
-    } else {
-      this.setResult(await this.gearStore.searchAll());
-    }
-  }
-
-  public async register(value: Array<Gear>) {
-    await this.gearStore.register(value);
-  }
-
-  public setKeyword(value: string) {
-    this.keyword = value;
-  }
-
-  private setResult(value: Array<Gear>) {
-    this.result = value;
-  }
-
-  public getResult() {
-    return this.result;
+  protected constructor(gearStore: GearStore) {
+    super(gearStore);
   }
 
   public async select(value: Gear) {
     await this.register([value]);
-    await this.search(this.keyword);
+    await this.search(this.getKeyword());
   }
 
   public async deselect(value: Gear) {
-    await this.gearStore.remove(value);
-    await this.search(this.keyword);
+    await this.remove(value);
+    await this.search(this.getKeyword());
   }
 }
 
