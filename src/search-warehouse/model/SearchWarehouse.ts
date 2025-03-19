84c587c308a91798fcf5f18c986fc5c09ce6cb0e
doublejbs <doublejbs@users.noreply.github.com>
2025-03-19 15:21:30 +0000

    deploy: 05be53b5514e675a79f7185986f117886edffc99

--- src/search-warehouse/model/SearchWarehouse.ts
+++ src/search-warehouse/model/SearchWarehouse.ts
@@ -1,37 +1,116 @@
 import Gear from '../../model/Gear';
-import app from '../../App';
-import GearStore from '../../firebase/GearStore';
-import Search from './Search';
+import { action, makeObservable, observable } from 'mobx';
+import SearchDispatcherType from './SearchDispatcherType';
+import SearchDispatcher from './SearchDispatcher';
 
-class SearchWarehouse extends Search {
+class SearchWarehouse {
   public static new() {
-    return new SearchWarehouse(app.getGearStore());
+    return new SearchWarehouse(SearchDispatcher.new());
   }
 
-  protected constructor(private readonly gearStore: GearStore) {
-    super();
+  @observable private keyword: string = '';
+  @observable private result: Array<Gear> = [];
+  @observable private loading = false;
+  @observable private hasMore = false;
+  private page = 0;
+
+  protected constructor(
+    private readonly searchDispatcher: SearchDispatcherType
+  ) {
+    makeObservable(this);
   }
 
   public async deselect(gear: Gear) {
-    await this.gearStore.remove(gear);
+    await this.searchDispatcher.remove(gear);
     await this.search(this.getKeyword());
   }
 
   public async select(gear: Gear) {
-    await this.gearStore.register([gear]);
+    await this.searchDispatcher.register([gear]);
     await this.search(this.getKeyword());
   }
 
-  public async searchList(
-    keyword: string
-  ): Promise<{ gears: Gear[]; hasMore: boolean }> {
-    return await this.gearStore.searchList(keyword);
+  public async search(keyword: string) {
+    this.clearPage();
+    this.setKeyword(keyword.trim());
+    this.setResult([]);
+    await this.executeSearch();
   }
 
-  public async searchListMore(
-    keyword: string
-  ): Promise<{ gears: Gear[]; hasMore: boolean }> {
-    return await this.gearStore.searchListMore(keyword);
+  public async searchMore() {
+    if (this.hasMore) {
+      await this.executeSearch();
+    }
+  }
+
+  private async executeSearch() {
+    this.setLoading(true);
+
+    if (this.keyword) {
+      const { gears, hasMore } = await this.searchDispatcher.searchList(
+        this.getKeyword(),
+        this.plusPage()
+      );
+
+      this.appendResult(gears);
+      this.setHasMore(hasMore);
+    } else {
+      this.setResult([]);
+    }
+    this.setLoading(false);
+  }
+
+  @action
+  private appendResult(value: Array<Gear>) {
+    this.result.push(...value);
+  }
+
+  @action
+  private setKeyword(value: string) {
+    this.keyword = value;
+  }
+
+  @action
+  private setResult(value: Array<Gear>) {
+    this.result = value;
+  }
+
+  public getResult() {
+    return this.result;
+  }
+
+  public getKeyword() {
+    return this.keyword;
+  }
+
+  @action
+  private setLoading(value: boolean) {
+    this.loading = value;
+  }
+
+  public isLoading() {
+    return this.loading;
+  }
+
+  public isEmpty() {
+    return !this.result.length;
+  }
+
+  @action
+  private setHasMore(value: boolean) {
+    this.hasMore = value;
+  }
+
+  public canLoadMore() {
+    return this.hasMore;
+  }
+
+  private clearPage() {
+    this.page = 0;
+  }
+
+  private plusPage() {
+    return this.page++;
   }
 }
 
