07dd105f9fe319c539cdc7f4a863c769b3d1fce2
jin <jinyong.suh@navercorp.com>
2025-04-17 23:18:06 +0900

    빈화면 추가

--- src/warehouse/component/WarehouseEmptyView.tsx
+++ src/warehouse/component/WarehouseEmptyView.tsx
@@ -8,6 +8,42 @@ const WarehouseEmptyView: FC = () => {
 
   return (
     <Layout>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '16px',
+          marginTop: '8px',
+          marginBottom: '16px',
+        }}
+      >
+        <div
+          style={{
+            fontWeight: '1000',
+            fontSize: '48px',
+            textAlign: 'center',
+            display: 'inline-block',
+            lineHeight: 1,
+            letterSpacing: '-4.5px',
+          }}
+        >
+          useless
+        </div>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          fontSize: '24px',
+          fontWeight: 'bold',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          paddingBottom: '53px',
+        }}
+      >
+        <span>장비를 추가해 주세요</span>
+      </div>
       <AddButtonView />
       <Bottom />
     </Layout>
