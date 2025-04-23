61bbb2ff396cbbee238fd3d271a8a531da5959f0
doublejbs <doublejbs@users.noreply.github.com>
2025-04-23 02:08:55 +0000

    deploy: c63aacd820578bb326914b67090c207b4f4568cf

---
+++ src/advertisement/KakaoAdView.tsx
@@ -0,0 +1,13 @@
+const KakaoAdView = () => {
+  return (
+    <ins
+      className='kakao_ad_area'
+      style={{ display: 'none' }}
+      data-ad-unit='DAN-xDk1Qq178A33DbF5'
+      data-ad-width='320'
+      data-ad-height='100'
+    ></ins>
+  );
+};
+
+export default KakaoAdView;
