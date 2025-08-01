94a5297a54b501ffab8963b8dbb11918f88a1fb9
doublejbs <doublejbs@users.noreply.github.com>
2025-08-01 16:24:01 +0000

    deploy: df86fe91a0d80b0ae7102356d61fec20c0b74a46

---
+++ src/bag-detail/component/FilterButtonView.tsx
@@ -0,0 +1,48 @@
+import { observer } from 'mobx-react-lite';
+import { FC, useRef } from 'react';
+import WarehouseFilter from '../../warehouse/model/WarehouseFilter';
+
+interface BagWithFilters {
+  toggleFilterWithScroll: (filter: WarehouseFilter) => void;
+}
+
+interface Props {
+  filter: WarehouseFilter;
+  bagDetail: BagWithFilters;
+}
+
+const FilterButtonView: FC<Props> = ({ filter, bagDetail }) => {
+  const isSelected = filter.isSelected();
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+    bagDetail.toggleFilterWithScroll(filter);
+  };
+
+  return (
+    <button
+      ref={ref}
+      key={filter.getName()}
+      className={'clickable'}
+      style={{
+        height: '32px',
+        borderRadius: '22px',
+        fontSize: '14px',
+        padding: '8px 16px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        whiteSpace: 'nowrap',
+        backgroundColor: isSelected ? 'black' : '#EBEBEB',
+        color: isSelected ? 'white' : 'black',
+      }}
+      onClick={handleClick}
+    >
+      {filter.getName()}
+    </button>
+  );
+};
+
+export default observer(FilterButtonView); 
