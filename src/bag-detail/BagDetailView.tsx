444a67fd300ce4af479329167c34341110f26781
doublejbs <doublejbs@users.noreply.github.com>
2025-08-02 01:32:18 +0000

    deploy: 4c430feb4c9d681e587b344e903e87b701cd3e93

--- src/bag-detail/BagDetailView.tsx
+++ src/bag-detail/BagDetailView.tsx
@@ -27,6 +27,10 @@ const BagDetailView: FC<Props> = ({ bagDetail }) => {
     navigate(`/bag/${bagDetail.getId()}/edit`, { state: { from: `/bag/${bagDetail.getId()}` } });
   };
 
+  const handleClickBack = () => {
+    navigate(-1);
+  };
+
   useEffect(() => {
     bagDetail.initialize();
   }, []);
@@ -72,23 +76,6 @@ const BagDetailView: FC<Props> = ({ bagDetail }) => {
       >
         <div
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            width: '100%',
-            backgroundColor: 'white',
-            paddingTop: '0.5rem',
-            gap: '0rem',
-          }}
-        >
-          <div style={{ position: 'relative' }}>
-            <BagDetailNameView bagDetail={bagDetail} />
-            <ShareButtonView bagDetail={bagDetail} />
-          </div>
-          <BagDetailDateView bagDetail={bagDetail} />
-        </div>
-        <div
-          style={{
             position: 'sticky',
             top: 0,
             width: '100%',
@@ -106,9 +93,42 @@ const BagDetailView: FC<Props> = ({ bagDetail }) => {
               fontSize: '1.8rem',
             }}
           >
+            <div style={{
+              position: 'absolute',
+              left: '0',
+              top: '0.5rem',
+              padding: '1rem',
+            }}
+            onClick={handleClickBack}
+            >
+           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.675 20.475C15.375 20.475 15.075 20.375 14.875 20.075L7.375 12.575C6.875 12.075 6.875 11.375 7.375 10.875L14.875 3.375C15.375 2.875 16.075 2.875 16.575 3.375C17.075 3.875 17.075 4.575 16.575 5.075L9.775 11.775L16.475 18.475C16.975 18.975 16.975 19.675 16.475 20.175C16.275 20.375 15.975 20.475 15.675 20.475Z" fill="#191F28"/>
+</svg>
+
+
+            </div>
             <FlipCounter value={weight} />
+            <ShareButtonView bagDetail={bagDetail} />
           </div>
         </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            width: '100%',
+            backgroundColor: 'white',
+            paddingTop: '0.5rem',
+            gap: '0rem',
+          }}
+        >
+          <div style={{ position: 'relative' }}>
+            <BagDetailNameView bagDetail={bagDetail} />
+           
+          </div>
+          <BagDetailDateView bagDetail={bagDetail} />
+        </div>
+        
 
           <BagDetailUselessDescriptionView bagDetail={bagDetail} />
           <div
