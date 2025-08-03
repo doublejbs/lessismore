723ee76a77e51d728ad0481f2ada5acb4dc25b2b
doublejbs <doublejbs@users.noreply.github.com>
2025-08-03 07:25:30 +0000

    deploy: 406b7ad7b34b57efa67fb05d8a26fc9780630161

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,17 @@
 import { FC, ReactNode } from 'react';
 
 interface Props {
+  vh?: boolean;
   children: ReactNode;
 }
 
-const Layout: FC<Props> = ({ children }) => {
+const Layout: FC<Props> = ({ children, vh = false }) => {
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column',
-        height: '100%',
+        height: vh ? '100vh' : '100%',
         width: '100%',
         padding: '0 20px 20px 20px',
         position: 'relative',
